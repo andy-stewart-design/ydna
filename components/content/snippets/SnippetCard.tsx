708bfe9fd43c3fdb42c1123067835315b7d3fc4f
Andy Stewart <andy.stewart1170@gmail.com>
2022-08-05 22:29:37 -0700

    added button animation

--- components/content/snippets/SnippetCard.tsx
+++ components/content/snippets/SnippetCard.tsx
@@ -2,36 +2,41 @@ import Link from "next/link";
 import NextImage from "next/future/image";
 import Button from "components/global/Button";
 import { type Snippet } from "contentlayer/generated";
+import { useState } from "react";
 
 interface Props {
   data: Snippet;
 }
 
 const SnippetCard = ({ data }: Props) => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
   return (
-    <Link href={`/snippets/${data.slug}`} key={data.slug}>
-      <a className="group relative flex flex-col items-start w-full mt-6 mb-8 bg-white bg-opacity-40 dark:bg-gray-800 dark:bg-opacity-20 border border-black/10 dark:border-white/20 rounded transition-colors duration-500 ease-out hover:bg-opacity-80 dark:hover:bg-opacity-50 hover:border-black/50 dark:hover:border-white/50 overflow-hidden">
-        <div className="relative w-full bg-black">
-          <NextImage
-            src={data.image}
-            className="relative w-full aspect-video group-hover:opacity-60 transition-opacity duration-500 ease-out"
-            width="1920"
-            height="1080"
-            sizes="40vw"
-            quality="60"
-            alt="foo"
-          ></NextImage>
-          <span className="absolute top-4 left-4 font-medium text-sm text-white px-3 py-1 bg-black/60 rounded-full backdrop-blur-md">
-            {data.framework}
-          </span>
-        </div>
-        <div className="relative flex flex-col items-start gap-y-3 grow p-6 pb-18">
-          <h3 className="font-bold text-2xl">{data.title}</h3>
-          <p className="text-sm">{data.summary}</p>
-          <Button focusable={false}></Button>
-        </div>
-      </a>
-    </Link>
+    <div onMouseEnter={increment}>
+      <Link href={`/snippets/${data.slug}`} key={data.slug}>
+        <a className="group relative flex flex-col items-start w-full mt-6 mb-8 bg-white bg-opacity-40 dark:bg-gray-800 dark:bg-opacity-20 border border-black/10 dark:border-white/20 rounded transition-colors duration-500 ease-out hover:bg-opacity-80 dark:hover:bg-opacity-50 hover:border-black/50 dark:hover:border-white/50 overflow-hidden">
+          <div className="relative w-full bg-black">
+            <NextImage
+              src={data.image}
+              className="relative w-full aspect-video group-hover:opacity-60 transition-opacity duration-500 ease-out"
+              width="1920"
+              height="1080"
+              sizes="40vw"
+              quality="60"
+              alt="foo"
+            ></NextImage>
+            <span className="absolute top-4 left-4 font-medium text-sm text-white px-3 py-1 bg-black/60 rounded-full backdrop-blur-md">
+              {data.framework}
+            </span>
+          </div>
+          <div className="relative flex flex-col items-start gap-y-3 grow p-6 pb-18">
+            <h3 className="font-bold text-2xl">{data.title}</h3>
+            <p className="text-sm">{data.summary}</p>
+            <Button focusable={false} trigger={count}></Button>
+          </div>
+        </a>
+      </Link>
+    </div>
   );
 };
 
--- components/global/Button.tsx
+++ components/global/Button.tsx
@@ -1,24 +1,49 @@
+import { timeline } from "motion";
+import { useEffect, useRef } from "react";
+
 interface Props {
   focusable?: boolean;
+  trigger?: number;
 }
 
-const Button = ({ focusable = true }: Props) => {
+const Button = ({ focusable = true, trigger = 0 }: Props) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current)
+      timeline([
+        [
+          ref.current,
+          { x: "200%" },
+          { duration: 0.25, easing: [0.33, 1, 0.68, 1] },
+        ],
+        [ref.current, { x: "-200%" }, { duration: 0.0001 }],
+        [
+          ref.current,
+          { x: "0%" },
+          { duration: 0.25, easing: [0.33, 1, 0.68, 1] },
+        ],
+      ]);
+  }, [trigger]);
+
   return (
     <button
-      className="absolute left-6 bottom-5 p-2 border border-black/10 dark:border-white/20 rounded-full transition-colors duration-500 ease-out group-hover:border-black/50 dark:group-hover:border-white/50"
+      className="absolute left-6 bottom-5 p-2 border border-black/10 dark:border-white/20 rounded-full transition-colors duration-500 ease-out group-hover:border-black/50 dark:group-hover:border-white/50 overflow-hidden"
       tabIndex={focusable ? 0 : -1}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 16 16"
-        xmlSpace="preserve"
-        className="w-4"
-      >
-        <path
-          d="m14.707 7.293-6-6-1.414 1.414L11.586 7H1v2h10.586l-4.293 4.293 1.414 1.414 6-6c.391-.391.391-1.023 0-1.414z"
-          fill="currentColor"
-        />
-      </svg>
+      <div ref={ref}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          xmlSpace="preserve"
+          className="w-4"
+        >
+          <path
+            d="m14.707 7.293-6-6-1.414 1.414L11.586 7H1v2h10.586l-4.293 4.293 1.414 1.414 6-6c.391-.391.391-1.023 0-1.414z"
+            fill="currentColor"
+          />
+        </svg>
+      </div>
     </button>
   );
 };
